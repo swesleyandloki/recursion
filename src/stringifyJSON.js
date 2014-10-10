555da52507b5193443caa3bac52c5b12dc1b6a96
SWES <swesleyandloki@gmail.com>
2014-10-10 14:52:43 +0200

    stringifies all stringifiable values, cannot handle nonstringifiable values

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,31 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var stringer = "\"";
+  if(typeof obj==='number' || typeof obj==='boolean'){return obj.toString();}
+  if(obj===null){return 'null';}
+  if(typeof obj==='string'){return "\"" + obj + "\"";}
+  if(obj===undefined || typeof obj==='function'){return undefined;}
+  if(Array.isArray(obj)){
+  	var stringArray = '';
+  	_.each(obj, function(el){
+  		var stringified = stringifyJSON(el);
+  		stringArray = stringArray.concat(stringified, ",");
+  	});
+  	stringArray = stringArray.slice(0, stringArray.length-1);
+  	var stringObj = "["+stringArray+"]";
+  	return stringObj;
+  }
+  if(!Array.isArray(obj) && typeof obj==='object'){
+  	var stringer = '';
+  	_.each(obj, function(el, key){
+  		var keyValString = stringifyJSON(key).concat(":", stringifyJSON(el));
+  		stringer = stringer.concat(keyValString, ",");
+  	});
+  	stringer = stringer.slice(0, stringer.length-1);
+  	var stringObj = "{"+stringer+"}";
+  	return stringObj;
+  }
+  
+
 };
