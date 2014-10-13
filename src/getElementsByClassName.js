44bdacb7dfe8468e6b4e7f279af1187b4e3638bd
SWES <swesleyandloki@gmail.com>
2014-10-13 16:09:08 +0100

    more attempts still not functional

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,23 +5,21 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  //establish place to look
-  var $placeToLook;
-  var foundElements = [];
-  
-
-  if($placeToLook===undefined){
-  	$placeToLook = $(document);
-  } else {
-  	var $classSelect = $("."+className);
-  	foundElements.push($classSelect);
-  	if($placeToLook.children().length>0){
-  		_.each($placeToLook.children(), function(el){
-  			$placeToLook = el;
-  			getElementsByClassName(className);
-  		});
-  	}else{
-  		$placeToLook = $placeToLook.next();
-  	}
-  } return foundElements;	
+	
+	var searchString = className;	
+	var $placeToLook;
+	var foundElements = [];
+	
+	if ($placeToLook === undefined){$placeToLook = $(document);}
+	if ($placeToLook.hasClass(className)){foundElements.push($placeToLook);}
+	if ($placeToLook.children() > 0){
+		_.each($placeToLook.children, function(el){
+			$placeToLook = el;
+			getElementsByClassName(searchString);
+		});
+	} else if ($placeToLook.next() !== undefined){
+		$placeToLook = $placeToLook.next();
+		getElementsByClassName(searchString);
+	}
+	return foundElements;
 };
